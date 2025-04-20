0b5573d723ea9b2be87a64ab001693d2807de864
Belal <bb5503344@gmail.com>
2025-04-21 00:20:01 +0200

    oo

--- e-commerce/src/Components/2-HomePage/SwiperC.jsx
+++ e-commerce/src/Components/2-HomePage/SwiperC.jsx
@@ -11,8 +11,7 @@ export default function App() {
   return (
     <>
       <Swiper
-        slidesPerView={1.5}
-        spaceBetween={30}
+        slidesPerView={1}
         centeredSlides={true}
         autoplay={{
           delay: 2500,
@@ -22,28 +21,28 @@ export default function App() {
         pagination={{
           clickable: true,
         }}
-        navigation={true}
+        navigation={true} 
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>
           <div className='swiper-slide'>
-            <img src="/img/img1.jpg" style={{ height: "100%", width: "100%", objectFit: "cover" }} alt="" />
+            <img src="/img/img1.jpg" style={{ height: "100%", width: "100%" }} alt="" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className='swiper-slide'>
-            <img src="/img/img2.jpg" style={{ height: "100%", width: "100%", objectFit: "cover" }} alt="" />
+            <img src="/img/img2.jpg" style={{ height: "100%", width: "100%" }} alt="" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className='swiper-slide'>
-            <img src="/img/img3.jpg" style={{ height: "100%", width: "100%", objectFit: "cover" }} alt="" />
+            <img src="/img/img3.jpg" style={{ height: "100%", width: "100%" }} alt="" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className='swiper-slide'>
-            <img src="/img/img5.jpg" style={{ height: "100%", width: "100%", objectFit: "cover" }} alt="" />
+            <img src="/img/img5.jpg" style={{ height: "100%", width: "100%" }} alt="" />
           </div>
         </SwiperSlide>
       </Swiper>
